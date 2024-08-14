d2f4b7df5948a8ffd80f6f451374ab9aed2cb176
elizap0300@gmail.com <elizap0300@gmail.com>
2024-08-14 17:58:04 +0000

    Edited files:     /src/functions.js

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,5 @@
-function getRandomIntInclusive(1000, 9999) {
-  const minCeiled = Math.ceil(min);
-  const maxFloored = Math.floor(max);
+function getRandomIntInclusive() {
+  const minCeiled = Math.ceil(1000);
+  const maxFloored = Math.floor(9999);
   return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
 }

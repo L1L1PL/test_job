597b770fb0d32ef81315bc67427de3bf863dab5d
elizap0300@gmail.com <elizap0300@gmail.com>
2024-08-21 11:39:32 +0000

    Edited files:     /src/scripts/functions.js

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -36,7 +36,54 @@ function checkNumber(num, guess) {
         $reactions.answer("Цифры не могут повторяться. Попробуй снова.");
     }else if (num == guess) {
         $reactions.answer("Ты выиграл! Хочешь еще раз?");
+    }else {
+        var bulls = 0;
+        var cows = 0;
+        var bull_list = [];
+        var cow_list = [];
+        var num = num.toString()
+        for (var i = 0; i < 4; i++) {
+            if (num.charAt(i) === guess.toString().charAt(i)) {
+                bulls++;
+                bull_list.push(num.charAt(i));
+        
+            } else if (guess.toString().indexOf(num.charAt(i)) !== -1) {
+                cows++;
+                cow_list.push(num.charAt(i));
+                }    
+            }
+    } return answer(bulls, cows, bull_list, cow_list)
+}   
+
+function ReturnWord(numb){
+    if (numb === 2) {
+      var word = 'две'
+    } else if (numb === 3) {
+      var word = 'три'
+    } else if (numb === 4) {
+      var word = 'четыре' 
+    } return(word)
+}
+
+function answer(bulls, cows, bull_list, cow_list){
+    var react_bull;
+    if (bulls === 0) {
+        var react_bull = bulls +' быков.';
+    } else if (bulls === 1) {
+        var react_bull = bulls + ' бык ' + "(одна цифра: «"+bull_list+"» - угадана вплоть до позиции).";
     } else {
-        $reactions.answer("Попытки");
+        var word = ReturnWord(bulls)
+        var react_bull = bulls + ' быка ' + "(" + word + " цифры: «"+bull_list.join('», «')+"» - угаданы вплоть до позиции).";
     }
+    
+    var react_cows;
+    if (cows=== 0) {
+        var react_cows = cows + ' коров и ';
+    } else if (cows === 1) {
+        var react_cows = cows + ' корова ' + "(одна цифра: «"+cow_list+"» - угадана на неверной позици) и ";
+    } else {
+        var word = ReturnWord(bulls)
+        var react_cows = cows + ' коровы ' + "(" + word + " цифры: «"+cow_list.join('», «')+"» - угаданы на неверных позициях) и ";
+    }
+    return($reactions.answer("Результат: " + react_cows + react_bull));
 }
